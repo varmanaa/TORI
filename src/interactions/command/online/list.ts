99625a5f2bca59f35314650ba5563c933ce83888
Chiitoi <vrajakul@gmail.com>
2023-04-23 18:25:21 +0200

    feat: add info embed to online command

--- src/interactions/command/online/list.ts
+++ src/interactions/command/online/list.ts
@@ -29,23 +29,25 @@ export const OnlineListCommand: CommandInteraction = {
 
         const date = interaction.getStringOption('date')
         const onlineGames = await client.database.readOnlineGamesByDate(interaction.guildId, date)
-        const embeds: Partial<APIEmbed>[] = onlineGames?.length
-            ? Array
-                .from({ length: Math.ceil(onlineGames.length / 5) }, (_, i) => onlineGames.slice(5 * i, 5 * (i + 1)))
-                .map((onlineGameChunk, i ) => {
-                    const description = onlineGameChunk
-                        .map(onlineGame => `Game ${ onlineGame.id } - [Tenhou link](${ onlineGame.url })`)
-                        .join('\n')
-                    const title = `Game${ onlineGameChunk.length === 1 ? '' : 's' } ${ (5 * i) + 1}${ onlineGameChunk.length === 1 ? '' : ` - ${ (5 * i) + onlineGameChunk.length }` } (of ${ onlineGames.length }) for ${ date }`
+        const embeds: Partial<APIEmbed>[] = [{ color: 0xF8F8FF, description: `${ onlineGames.length ? onlineGames.length : 'No' } online game(s) found for ${ date }.` }]
+        
+        if (onlineGames.length)
+            embeds.push(
+                ...Array
+                    .from({ length: Math.ceil(onlineGames.length / 5) }, (_, i) => onlineGames.slice(5 * i, 5 * (i + 1)))
+                    .map((onlineGameChunk, i ) => {
+                        const description = onlineGameChunk
+                            .map(onlineGame => `Game ${ onlineGame.id } - [Tenhou link](${ onlineGame.url })`)
+                            .join('\n')
+                        const title = `Game${ onlineGameChunk.length === 1 ? '' : 's' } ${ (5 * i) + 1}${ onlineGameChunk.length === 1 ? '' : ` - ${ (5 * i) + onlineGameChunk.length }` } (of ${ onlineGames.length }) for ${ date }`
 
-                    return {
-                        color: 0xF8F8FF,
-                        description,
-                        title
-                    }
-                })
-
-            : [{ color: 0xF8F8FF, description: `No online games found for ${ date }.` }]
+                        return {
+                            color: 0xF8F8FF,
+                            description,
+                            title
+                        }
+                    })
+            )
 
         await interaction.updateReply({ embeds })
     }
